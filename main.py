31ef69dfe1ce575992be916178fc5a7e0e4a2db9
Josué Herrera <54568984+josh-hrr@users.noreply.github.com>
2022-12-19 18:16:57 +0000

    added a 3th challenge

--- main.py
+++ main.py
@@ -6,3 +6,22 @@ result = ''
 for i in range(len(var1List)-1, -1, -1):
     result += var1List[i] + ' '
 print(result)
+
+
+#reverse characters
+test1 = 'test'
+print(test1[::-1])
+
+#reverse characters in array without changing the order
+myString = 'Python is cool'
+splitedArray = myString.split()
+newResult = []
+i = 0
+
+while i < len(splitedArray):
+    newResult.append(splitedArray[i][::-1])
+    i = i + 1
+ 
+print(newResult)
+result3 = ' '.join(newResult)
+print(result3)
