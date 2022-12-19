71bcd2a115bdfd05744bc6cafc3d2822cf664560
Josué Herrera <54568984+josh-hrr@users.noreply.github.com>
2022-12-19 22:05:02 +0000

    latest version

--- main.py
+++ main.py
@@ -103,3 +103,15 @@ def testing(a, b):
 
 newSum = testing(1,2)
 print(testing(1,2))
+
+#factorial
+
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+print(factorial(5))
+
+#*args **kwargs
