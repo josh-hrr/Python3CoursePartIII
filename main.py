b301598c46eb1c7d94116d9eca83d6477e398d12
Josué Herrera <54568984+josh-hrr@users.noreply.github.com>
2022-12-19 20:39:23 +0000

    adding #find a substring and #tree

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 var1 = 'hello world this is a test'
 var1List = var1.split() 
 counter = 0 
@@ -25,3 +26,41 @@ while i < len(splitedArray):
 print(newResult)
 result3 = ' '.join(newResult)
 print(result3)
+
+
+
+#count the characters in a string
+
+myNewString = 'aaabbccccccccasdfjsadfl'
+dic = {} 
+for i in myNewString:
+    if i in dic.keys():
+      dic[i] = dic[i] + 1
+    else:
+      dic[i] = 1
+    print(dic) 
+
+for k, v in dic.items():
+    print('{}={} times '.format(k,v))
+'''
+#tree
+
+rows = 6 
+for i in range(1, rows):
+    print(' ' * (rows - i), end='')
+    print('* ' * i)
+
+#find a substring
+
+StringTest = 'Take up one idea and make that idea your life'
+subString = 'idea'
+StringToArray = StringTest.split()
+counter = 0
+print(StringTest.find('idea'))
+
+for i in StringToArray:
+    if i == subString:
+        counter = counter + 1
+
+
+print('Substring is found in: ', counter, ' times')
